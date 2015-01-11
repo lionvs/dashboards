2e806b502492476cffd96a6864a5de70746cc798
VitaliiKulyk <vetal_kulyk@i.ua>
2015-01-12 00:59:46 +0200

    Merge branch 'master' of https://github.com/m213/ITA-dashboards

--- project/DataVis/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DataVis.MvcApplication" Language="C#" %>
--- project/DataVis/Scripts/events.js
+++
@@ -1,4 +0,0 @@
-var events = {
-    updatedDataSource: 'updatedDataSource',
-    updatedChartConfig: 'updatedChartConfig'
-}
