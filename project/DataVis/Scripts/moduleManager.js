2dfcfe9d5ad8656fd435d1aa527eba96d980a331
mplakhtiy <mplakhtiy@gmail.com>
2015-01-22 12:54:42 +0200

    add moduleManager

---
+++ project/DataVis/Scripts/moduleManager.js
@@ -0,0 +1,55 @@
+﻿var moduleManager = function () {
+    var runModule = function () {
+        return {
+            UploadData: function () {
+                core.registerModule(moduleUploadData);
+                core.startModule(moduleUploadData, document.getElementById("uploadData"));
+            },
+            DataTable: function () {
+                core.registerModule(moduleDataTable);
+                core.startModule(moduleDataTable, document.getElementById("dataTable"));
+            },
+
+
+            HighChart: function () {
+                core.registerModule(moduleHighChart);
+                core.startModule(moduleHighChart, document.getElementById("chart"));
+            },
+            ChartConfig: function () {
+                core.registerModule(moduleChartConfig);
+                core.startModule(moduleChartConfig, document.getElementById("chartConfig"));
+            },
+            Filters: function () {
+                core.registerModule(moduleFilters);
+                core.startModule(moduleFilters, document.getElementById("filter"));
+            }
+        }
+    }
+    var runningModules = [];
+    return {
+        addModule: function (moduleInstance, element) {
+            runningModules.push({
+                module: moduleInstance,
+                element: element
+            });
+        },
+        removeModule: function (element) {
+            runningModules = _.filter(runningModules, function (obj) {
+                return obj.element !== element;
+            });
+        },
+        setConfig: function (element, config) {
+            var moduleWithinCurrentElement = _.find(runningModules, function (obj) {
+                return obj.element === element;
+            });
+            moduleWithinCurrentElement.module.setConfig(config);
+        },
+        getConfig: function (element) {
+            var moduleWithinCurrentElement = _.find(runningModules, function (obj) {
+                return obj.element === element;
+            });
+            return moduleWithinCurrentElement.module.getConfig();
+        },
+        runModule: runModule()
+    }
+}();
