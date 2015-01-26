25a9e380ed9aa24bf1243d77d82cc0bb9aae4fa2
byaremchyshyn <batonlct@gmail.com>
2015-01-26 17:25:20 +0200

    added interface for dashboard save

---
+++ project/DataVis/Scripts/angularControllers/headerDashboardSelector.js
@@ -0,0 +1,18 @@
+﻿var globalConfig = {
+    dashboard: ""
+}
+
+application.controller("headerDashboardSelector", function ($scope) {
+
+    $scope.globalConfig = globalConfig;
+
+    $scope.dashboardOptions = [
+        "dashboard1",
+        "dashboard2",
+        "dashboard3",
+        "dashboard4",
+        "dashboard5",
+        "dashboard6",
+        "dashboard7"
+    ];
+});
