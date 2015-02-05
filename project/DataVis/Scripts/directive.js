7fc3951f4c577014da0d59a40a7df2d40b282a5a
lionvs <lionvs13@gmail.com>
2015-02-05 02:14:38 +0200

    focusing widget

--- project/DataVis/Scripts/directive.js
+++ project/DataVis/Scripts/directive.js
@@ -1,9 +1,34 @@
 ﻿var directive = {
     create: function (module, classTitle, htmlTemplateUrl) {
+        var widgetFocused = false;
+        var zIndexOfElement = 0;
+
+        function onWidgetUnFocused(element) {
+            $('#overlay').fadeOut('fast', function () {
+                element.zIndex(zIndexOfElement);
+            });
+            widgetFocused = false;
+        }
+
+        function onWidgetFocused(element) {
+            zIndexOfElement = element.zIndex();
+            element.zIndex(9999);
+            $('#overlay').fadeIn('slow');
+            widgetFocused = true;
+        }
+
         function link(scope, element, attrs) {
             scope.widgetName = classTitle;
             scope.closeWidget = function() {
                 core.stopWidget(element);
+                if (widgetFocused)
+                    onWidgetUnFocused(element);
+            }
+            scope.focusWidget = function () {
+                if (widgetFocused) 
+                    onWidgetUnFocused(element);
+                else 
+                    onWidgetFocused(element);
             }
         }
 
