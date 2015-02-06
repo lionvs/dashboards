944f6a3be4df784667f11d4743627c50b76ff72d
lionvs <lionvs13@gmail.com>
2015-02-06 12:52:49 +0200

    collapse windget(2)

--- project/DataVis/Scripts/directive.js
+++ project/DataVis/Scripts/directive.js
@@ -1,10 +1,11 @@
-﻿var directive = {
-    create: function (module, classTitle, htmlTemplateUrl) {
+﻿var directive = function() {
+
+    
         var widgetFocused = false;
         var zIndexOfElement = 0;
 
         function onWidgetUnFocused(element) {
-            $('#overlay').fadeOut('fast', function () {
+            $('#overlay').fadeOut('fast', function() {
                 element.zIndex(zIndexOfElement);
             });
             widgetFocused = false;
@@ -17,28 +18,32 @@
             widgetFocused = true;
         }
 
-        function link(scope, element, attrs) {
-            scope.widgetName = classTitle;
-            scope.closeWidget = function() {
-                core.stopWidget(element);
-                if (widgetFocused)
-                    onWidgetUnFocused(element);
+        return {
+            create: function(module, classTitle, htmlTemplateUrl) {
+                module.directive(classTitle, function($compile) {
+                    return {
+                        restrict: 'C',
+                        templateUrl: htmlTemplateUrl,
+                        scope: true,
+                        link: function(scope, element, attrs) {
+                            scope.widgetName = classTitle;
+                            scope.closeWidget = function() {
+                                core.stopWidget(element);
+                                if (widgetFocused)
+                                    onWidgetUnFocused(element);
+                            }
+                            scope.focusWidget = function() {
+                                if (widgetFocused)
+                                    onWidgetUnFocused(element);
+                                else
+                                    onWidgetFocused(element);
+                            }
+                            scope.collapseWidget = function () {
+                                $(element).animate({ 'height': '30px' }, 500);
+                            }
+                        }
+                    }
+                });
             }
-            scope.focusWidget = function () {
-                if (widgetFocused) 
-                    onWidgetUnFocused(element);
-                else 
-                    onWidgetFocused(element);
-            }
-        }
-
-        module.directive(classTitle, function ($compile) {
-            return {
-                restrict: 'C',
-                templateUrl: htmlTemplateUrl,
-                scope: true,
-                link: link
-            }
-        });
     }
-}
+}();
