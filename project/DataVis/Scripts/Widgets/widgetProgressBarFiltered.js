05123675f16a28e024fa67337d238b7ff194f1a1
lionvs <lionvs13@gmail.com>
2015-02-25 02:07:36 +0200

    Added new widget Progress Bar
    It shows how many percent of specific data have been filtered

---
+++ project/DataVis/Scripts/Widgets/widgetProgressBarFiltered.js
@@ -0,0 +1,114 @@
+﻿var widgetProgressBarFiltered = function () {
+
+    var schemaItemForEverything = "Everything";
+
+    function getDefaultConfig() {
+        return {
+            width: '250px',
+            height: '200px',
+            key: schemaItemForEverything,
+            listProgressBars: []
+        }
+    }
+
+    function countPercentWithKey(key, listOfValuesCurrent, listOfValuesOriginal) {
+        var numberCurrent = (_.filter(listOfValuesCurrent,function (item) { return item === key; })).length;
+
+        var numberOriginal = (_.filter(listOfValuesOriginal, function (item) { return item === key; })).length;
+
+        return 100 - ((numberCurrent * 100) / numberOriginal);
+    }
+
+    function countPercent(listOfValuesCurrent, listOfValuesOriginal) {
+        var numberCurrent = listOfValuesCurrent.length;
+
+        var numberOriginal = listOfValuesOriginal.length;
+
+        return 100 - ((numberCurrent * 100) / numberOriginal);
+    }
+
+    function createListProgressBars(sandbox, config) {
+        var originalData = sandbox.getOriginalDatasource().data;
+        var currentData = sandbox.getDatasource().data;
+
+        var listOfValuesOriginal = _.map(originalData, function (item) {
+            return item[config.key];
+        });
+
+        var listOfValuesCurrent = _.map(currentData, function (item) {
+            return item[config.key];
+        });
+
+        var uniqueListOfValues = _.uniq(listOfValuesOriginal);
+
+        if (config.key === schemaItemForEverything) {
+            var percent = countPercent(listOfValuesCurrent, listOfValuesOriginal);
+            config.listProgressBars = [{ name: schemaItemForEverything, percent: percent, style: { 'width': percent + '%' } }];
+            return;
+        }
+
+        config.listProgressBars = _.map(uniqueListOfValues, function (item) {
+            var percent = countPercentWithKey(item,listOfValuesCurrent,listOfValuesOriginal);
+            return { name: item, percent: percent, style: { 'width': percent +'%' } };
+        });
+    }
+
+    function fillHtmlTemplate(sandbox, data, schema,config) {
+        var angular = sandbox.require('angular');
+        var $scope = angular.element(sandbox.getContainer()).scope();
+        _.defer(function () {
+            $scope.schemaOptions = [schemaItemForEverything].concat(sandbox.getOriginalDatasource().schema);
+            $scope.config = config;
+            $scope.createListProgressBars = function () {
+                createListProgressBars(sandbox, config);
+            };
+            $scope.$digest();
+        });
+    }
+
+    function setResizable(element, config, $) {
+        if (config)
+            $(element).width(config.width).height(config.height);
+        $(element).resizable({
+            resize: function (e, ui) {
+                config.width = ui.size.width;
+                config.height = ui.size.height;
+            }
+        });
+    }
+
+    function createProgressBars(sandbox, config) {
+        var element = sandbox.getContainer();
+        var dataSource = sandbox.getDatasource();
+        var $ = sandbox.require('JQuery');
+
+        createListProgressBars(sandbox, config);
+        fillHtmlTemplate(sandbox, dataSource.data, dataSource.schema,config);
+        setResizable(element, config, $);
+    }
+
+    return {
+        name: "progressBarFiltered",
+        init: function(sandbox) {
+            var config = getDefaultConfig(sandbox);
+
+            createProgressBars(sandbox, config);
+            sandbox.listen(events.updatedDataSource, function () {
+                createProgressBars(sandbox, config);
+            });
+
+            return {
+                setConfig: function(newConfig) {
+                    config = newConfig;
+                    createProgressBars(sandbox, config);
+                },
+                getConfig: function() {
+                    return config;
+                }
+            }
+        },
+        imgUrl: "DataTable",
+        title: "This progress bar shows how many percent of specific data has been filtered"
+}
+}();
+
