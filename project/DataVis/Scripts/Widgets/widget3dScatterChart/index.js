435e2efe241cfa7f3cb06ad5301c312f6d9d248d
lionvs <lionvs13@gmail.com>
2016-04-24 00:26:54 +0300

    3d scater chart

--- project/DataVis/Scripts/Widgets/widget3dScatterChart/index.js
+++ project/DataVis/Scripts/Widgets/widget3dScatterChart/index.js
@@ -30,6 +30,8 @@
         return {
             title: "Title",
             xAxis: schema[0],
+            yAxis: schema[1],
+            zAxis: schema[2],
             seriesName: schema[1],
             seriesData: schema[2],
             chartType: "spline",
@@ -94,7 +96,7 @@
         var series = _.map(seriesObject, function (num) {
             var currentSeriesName;
             if (_.isNumber(name)) { currentSeriesName = name.toString(); } else { currentSeriesName = num[0]; }
-            return { name: currentSeriesName, data: num[1] };
+            return { name: currentSeriesName, colorByPoint: true, data: num[1] };
         });
         return series;
     }
@@ -193,6 +195,19 @@
         title: "This is 3d chart"
     }
 
+    function generateAxis(axisData, text) {
+        return _.every(axisData, function (d) { return _.isNumber(d) })
+            ? {
+                title: { text: text },
+                min: _.min(axisData),
+                max: _.max(axisData)
+            }
+            : {
+                title: { text: text },
+                categories: _.uniq(axisData)
+            }
+    }
+
     function defaultChart(element, datasource, configChart) {
         var xAxis = getUniqueValues(datasource.data, configChart.xAxis);
         var seriesNames = getUniqueValues(datasource.data, configChart.seriesName);
@@ -211,6 +226,15 @@
                 ]
             };
         });
+        var yAxis = generateAxis(datasource.data.map(function (d) { return d[configChart.yAxis] }), configChart.yAxis);
+        var zAxis = generateAxis(datasource.data.map(function (d) { return d[configChart.zAxis] }), configChart.zAxis);
+        var xAxis = { categories: xAxis, title: { text: configChart.xAxis } };
+
+        function dataGenerator(axisData, value) {
+            return axisData.categories ? axisData.categories.indexOf(value) : value;
+        }
+
+        var data = datasource.data.map(function (d) { return [dataGenerator(d[configChart.xAxis]), dataGenerator(d[configChart.yAxis]), dataGenerator(d[configChart.zAxis])] });
 
         $(element).highcharts({
             chart: {
@@ -231,10 +255,7 @@
                 }
             },
             title: {
-                text: 'Draggable box'
-            },
-            subtitle: {
-                text: 'Click and drag the plot area to rotate in space'
+                text: 'Title'
             },
             plotOptions: {
                 scatter: {
@@ -243,28 +264,16 @@
                     depth: 10
                 }
             },
-            yAxis: {
-                min: 0,
-                max: 10,
-                title: null
-            },
-            xAxis: {
-                min: 0,
-                max: 10,
-                gridLineWidth: 1
-            },
-            zAxis: {
-                min: 0,
-                max: 10,
-                showFirstLabel: false
-            },
+            yAxis: yAxis,
+            xAxis: xAxis,
+            zAxis: zAxis,
             legend: {
                 enabled: false
             },
             series: [{
-                name: 'Reading',
+                name: 'Data',
                 colorByPoint: true,
-                data: [[1, 6, 5], [8, 7, 9], [1, 3, 4], [4, 6, 8], [5, 7, 7], [6, 9, 6], [7, 0, 5], [2, 3, 3], [3, 9, 8], [3, 6, 5], [4, 9, 4], [2, 3, 3], [6, 9, 9], [0, 7, 0], [7, 7, 9], [7, 2, 9], [0, 6, 2], [4, 6, 7], [3, 7, 7], [0, 1, 7], [2, 8, 6], [2, 3, 7], [6, 4, 8], [3, 5, 9], [7, 9, 5], [3, 1, 7], [4, 4, 2], [3, 6, 2], [3, 1, 6], [6, 8, 5], [6, 6, 7], [4, 1, 1], [7, 2, 7], [7, 7, 0], [8, 8, 9], [9, 4, 1], [8, 3, 4], [9, 8, 9], [3, 5, 3], [0, 2, 4], [6, 0, 2], [2, 1, 3], [5, 8, 9], [2, 1, 1], [9, 7, 6], [3, 0, 2], [9, 9, 0], [3, 4, 8], [2, 6, 1], [8, 9, 2], [7, 6, 5], [6, 3, 1], [9, 3, 1], [8, 9, 3], [9, 1, 0], [3, 8, 7], [8, 0, 0], [4, 9, 7], [8, 6, 2], [4, 3, 0], [2, 3, 5], [9, 1, 4], [1, 1, 4], [6, 0, 2], [6, 1, 6], [3, 8, 8], [8, 8, 7], [5, 5, 0], [3, 9, 6], [5, 4, 3], [6, 8, 3], [0, 1, 5], [6, 7, 3], [8, 3, 2], [3, 8, 3], [2, 1, 6], [4, 6, 7], [8, 9, 9], [5, 4, 2], [6, 1, 3], [6, 9, 5], [4, 8, 2], [9, 7, 4], [5, 4, 2], [9, 6, 1], [2, 7, 3], [4, 5, 4], [6, 8, 1], [3, 4, 0], [2, 2, 6], [5, 1, 2], [9, 9, 7], [6, 9, 9], [8, 4, 3], [4, 1, 7], [6, 2, 5], [0, 4, 9], [3, 5, 9], [6, 9, 1], [1, 9, 2]]
+                data: datasource.data.map(function (d) { return [dataGenerator(xAxis, d[configChart.xAxis]), dataGenerator(yAxis, d[configChart.yAxis]), dataGenerator(zAxis, d[configChart.zAxis])] })
             }]
         });
         var chart = $(element).highcharts();
